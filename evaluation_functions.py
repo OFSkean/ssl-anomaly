2f67b81e24c2b590c09bb3c8d6e18f3c00aace77
Oscar Skean <ofskean727@gmail.com>
2025-01-13 15:30:44 -0500

    addde lidar metric

--- evaluation_functions.py
+++ evaluation_functions.py
@@ -61,3 +61,64 @@ def renyi_rankme(Z: torch.Tensor, alpha: float = 2.0, eps: float = 1e-6) -> floa
         rank_me_score = -1
 
     return rank_me_score
+
+def compute_LDA_matrix(Z, return_within_class_scatter=False):
+    """
+    Compute the LDA matrix as defined in the LIDAR paper.
+
+    Args:
+        Z (torch.Tensor): Tensor of augmented prompts.
+        return_within_class_scatter (bool): Whether to return the within-class scatter matrix.
+
+    Returns:
+        torch.Tensor: The computed LDA matrix or within-class scatter matrix.
+    """
+    # Z is tensor that is NUM_SAMPLES x NUM_AUGMENTATIONS x D
+    NUM_SAMPLES, NUM_AUGMENTATIONS, D = Z.shape
+
+    delta = 1e-4
+
+    dataset_mean = torch.mean(Z, dim=(0, 1)).squeeze() # D
+    class_means = torch.mean(Z, dim=1) # NUM_SAMPLES x D
+
+    # Equation 1 in LIDAR paper
+    between_class_scatter = torch.zeros((D, D)).to(Z.device)
+    for i in range(NUM_SAMPLES):
+        between_class_scatter += torch.outer(class_means[i] - dataset_mean, class_means[i] - dataset_mean)
+    between_class_scatter /= NUM_SAMPLES
+
+    # Equation 2 in LIDAR paper
+    within_class_scatter = torch.zeros((D, D)).to(Z.device)
+    for i in range(NUM_SAMPLES):
+        for j in range(NUM_AUGMENTATIONS):
+            within_class_scatter += torch.outer(Z[i, j] - class_means[i], Z[i, j] - class_means[i])
+    within_class_scatter /= (NUM_SAMPLES * NUM_AUGMENTATIONS)
+    within_class_scatter += delta * torch.eye(D).to(Z.device)
+
+    if return_within_class_scatter:
+        return within_class_scatter 
+    
+    # Equation 3 in LIDAR paper
+    eigs, eigvecs = torch.linalg.eigh(within_class_scatter)
+    within_sqrt = torch.diag(eigs**(-0.5))
+    fractional_inverse = eigvecs @ within_sqrt @ eigvecs.T
+    LDA_matrix = fractional_inverse @ between_class_scatter @ fractional_inverse
+
+    return LDA_matrix
+
+def compute_lidar(Z, alpha=1, return_within_scatter=False):
+    """
+    Compute the LIDAR metric for hidden states.
+
+    Args:
+        hidden_states (torch.Tensor): The hidden states to compute LIDAR for. Must be of shape NUM_SAMPLES x NUM_AUGMENTATIONS x D, 
+                                             where NUM_AUGMENTATIONS is preferably high (50ish).
+        alpha (float): The alpha parameter for entropy calculation.
+        return_within_scatter (bool): Whether to return the within-class scatter matrix.
+
+    Returns:
+        float: The computed LIDAR metric.
+    """
+    lda_matrix = compute_LDA_matrix(Z.double(), return_within_class_scatter=return_within_scatter)
+    lda_rankme = rankme(lda_matrix, alpha=alpha)
+    return lda_rankme
